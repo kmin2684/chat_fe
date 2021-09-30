f29de13bca87ed0eb78f8b42285627be07478eec
kmin2684 <65061813+kmin2684@users.noreply.github.com>
2021-09-30 00:30:50 -0600

    multiple profile picture stack

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -10,6 +10,8 @@ import {
     useHistory,
   } from "react-router-dom";
 
+  import {StringToColor} from '../App'
+
 export default function Chat({room, setChatHistoryProp, chat, userInfo}) {
     const history = useHistory();
     const path = "/room/" + chat.id;
@@ -17,8 +19,27 @@ export default function Chat({room, setChatHistoryProp, chat, userInfo}) {
     : chat.members.length === 2 ? chat.members.find(member => member !== userInfo.username)
     : undefined
 
+    let otherMembers = chat?.members.filter(member => member !== userInfo.username);
+    
+    if (otherMembers?.length > 1) {
+      otherMembers = otherMembers.slice(0, 2);
+    }
 
-    return <div className='chat' onClick={()=>history.push(path)}>{chatName}</div>
+    return <>
+      <div className='chat' onClick={()=>history.push(path)}>
+        {/* {chat.member.length < 3? chat.members.map(member => member
+          chat.members.map(member => )} */}
+        
+  
+        {otherMembers.length > 1 ? (
+          <div className = 'userIcon group'> 
+          {otherMembers.map(member => <div className='userIcon small' style = {{backgroundColor: StringToColor(member)}}> {member[0]} </div>)}
+          </div>
+          )
+        : otherMembers.map(member => <div className='userIcon' style = {{backgroundColor: StringToColor(member)}}> {member[0]} </div>)}
+        {chatName}
+      </div>
+    </>
     // return <Link to={path}>
     //     <div>Chat name: {chat.name}</div>
     // </Link>;
