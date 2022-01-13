8357df5439a558d33b98e3e138c4a2cdc60536e9
kmin2684 <65061813+kmin2684@users.noreply.github.com>
2022-01-13 16:22:38 -0700

    time formatting function added for last message in Chat.jsx

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -8,6 +8,24 @@ import { useSelector } from "react-redux";
 import './Chat.scss';
 
 
+function formatTime(timeInput) {
+  const current = new Date();
+  const time = new Date(timeInput);
+  const monthMap = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const month = monthMap[time.getMonth()];
+
+  if (current.getFullYear() !== time.getFullYear()) return `${month} ${time.getDate()} ${time.getFullYear()}`
+  else if (current.getMonth() !== time.getMonth()) return `${month} ${time.getDate()}`;
+  else if (current.getDate() === time.getDate()) {
+    if (time.getHours() === 0) return `${12}:${time.getMinutes()} AM`;
+    else if (time.getHours() < 12) return `${time.getHours()}:${time.getMinutes()} AM`;
+    else if (time.getHours() === 12) return `${time.getHours()}:${time.getMinutes()} PM`;
+    return `${time.getHours()-12}:${time.getMinutes()} PM`;
+  }
+  return `${month} ${time.getDate()}`;
+}
+
+
 export default function Chat({chat}) {
     const history = useHistory();
     const userInfo = useSelector(state => state.userInfo);
@@ -36,7 +54,12 @@ export default function Chat({chat}) {
           <div>{chatName}</div>
           <div style={{display: 'flex'}}>
             <div className='content'>{chat.last_message?.content}</div>
-            <div className='time' >{chat.last_message?.time}</div>
+            <div className='time' >
+              {chat.last_message?.time && 
+              `· ${formatTime(chat.last_message.time)}`
+              }
+              
+              </div>
           </div> 
 
         </div>
