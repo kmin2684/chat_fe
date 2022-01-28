c963526604e25d02ed0e8a9b29a856e201c9e8ea
kmin2684 <65061813+kmin2684@users.noreply.github.com>
2022-01-28 02:47:56 -0700

    updated time display function so that minutes follow 0 when needed

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -13,14 +13,15 @@ function formatTime(timeInput) {
   const time = new Date(timeInput);
   const monthMap = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   const month = monthMap[time.getMonth()];
+  const minute = time.getMinutes() > 9 ? time.getMinutes() : `0${time.getMinutes()}`;
 
   if (current.getFullYear() !== time.getFullYear()) return `${month} ${time.getDate()} ${time.getFullYear()}`
   else if (current.getMonth() !== time.getMonth()) return `${month} ${time.getDate()}`;
   else if (current.getDate() === time.getDate()) {
-    if (time.getHours() === 0) return `${12}:${time.getMinutes()} AM`;
-    else if (time.getHours() < 12) return `${time.getHours()}:${time.getMinutes()} AM`;
-    else if (time.getHours() === 12) return `${time.getHours()}:${time.getMinutes()} PM`;
-    return `${time.getHours()-12}:${time.getMinutes()} PM`;
+    if (time.getHours() === 0) return `${12}:${minute} AM`;
+    else if (time.getHours() < 12) return `${time.getHours()}:${minute} AM`;
+    else if (time.getHours() === 12) return `${time.getHours()}:${minute} PM`;
+    return `${time.getHours()-12}:${minute} PM`;
   }
   return `${month} ${time.getDate()}`;
 }
